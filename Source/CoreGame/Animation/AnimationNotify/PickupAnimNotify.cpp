5592c6c1cbc9bafee4d3c45ec03653951d2425a5
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-07-31 22:20:26 +0530

    Moved Animation code

--- Source/CoreGame/Animation/AnimationNotify/PickupAnimNotify.cpp
+++
@@ -1,9 +0,0 @@
-/******************************************************************************
-* Project Core - Generic UE Project
-* Copyright (c) [2023] [ Joydip chakraborty ]
-* This class is part of the ProjectCore open-source project. 
-* ******************************************************************************/
-
-
-#include "Animation/AnimationNotify/PickupAnimNotify.h"
-
